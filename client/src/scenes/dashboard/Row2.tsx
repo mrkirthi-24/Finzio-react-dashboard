2345a0956236cc26e9a2a5208bc368b8ddf56cd3
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-05-29 23:46:57 +0530

    Build row2 grid-d chart

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,8 +1,29 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useTheme } from "@emotion/react";
+import { useGetKpisQuery } from "@/state/api";
+import { useMemo } from "react";
 
 type Props = {};
 
 const Row2 = (props: Props) => {
+  const { palette } = useTheme();
+  const { data: operationalData } = useGetKpisQuery();
+
+  const operationalExpenses = useMemo(() => {
+    return (
+      operationalData &&
+      operationalData[0].monthlyData.map(
+        ({ month, operationalExpenses, nonOperationalExpenses }) => {
+          return {
+            name: month.substring(0, 3),
+            operationalExpenses: operationalExpenses,
+            nonOperationalExpenses: nonOperationalExpenses,
+          };
+        }
+      )
+    );
+  }, [operationalData]);
+
   return (
     <>
       <DashboardBox gridArea="d"></DashboardBox>
